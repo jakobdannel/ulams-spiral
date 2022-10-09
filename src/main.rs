8b44afb728615c2521b94cc1d47d7f83ccdfa9d7
jakobdannel <jakob.dannel@t-online.de>
2022-10-09 16:12:48 +0200

    Added height and width input via console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::*;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Please enter a width:");
+    let mut input: String = String::new();
+
+    let mut width: usize = 100;
+    stdout().flush().expect("flush");
+    match stdin().read_line(&mut input) {
+        Ok(_n) => width = input.trim().parse().expect("No number"),
+        Err(e) => println!("error: {}", e),
+    }
+    input.clear();
+    print!("Please enter a height:");
+    
+    let mut height: usize = 100;
+    stdout().flush().expect("flush");
+    match stdin().read_line(&mut input) {
+        Ok(_n) => height = input.trim().parse().expect("No number"),
+        Err(e) => println!("error: {}", e),
+    }
+    input.clear();
+
+    println!("The generated image will be {} pixels wide and {} pixels tall.", width, height);   
 }
